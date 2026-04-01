54399f4e7b4478dcd6a0b361e2d8ec4cea462aa0
Dev <dkothari703@gmail.com>
2026-04-02 00:25:10 +0530

    added factorial of NCR

--- cpp2.cpp
+++ cpp2.cpp
@@ -1,9 +1,24 @@
 #include<iostream>
-using namespace std;
+using namespace std; // factorial of nCr
+int factorial(int n ) {
+   int fact= 1;
+   if(n==0 || n==1) return 1;
+   for(int i = 2 ; i<=n; i++) {
+      fact*=i;
+   }
+   return fact;
+}
+int nCr(int n , int r) {
+   int fact_N=factorial(n);
+   int fact_R=factorial(r);
+   int fact_NR=factorial(n-r);
+int  ncr= factorial(n) / factorial(r) * factorial(n-r);
+return ncr;
+}
+
  int main() {
-    string str;
-    cout<<" enter the string :";
-    getline(cin,str);
-    cout<<str<<"";
-    return 0;
+   int n = 4, r=3;
+  cout<< nCr(n,r)<<endl;
+  return 0;
+  
  }
