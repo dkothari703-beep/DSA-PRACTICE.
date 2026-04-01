3752565ce913e426421c3f7d12460fa04382fcf0
Dev <dkothari703@gmail.com>
2026-04-02 00:10:43 +0530

    added factorial of n

--- cpp1.cpp
+++ cpp1.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
-using namespace std;
+using namespace std; // factorial of n
+int factorialN(int n ) {
+   int fact= 1;
+   if(n==0 || n==1) return 1;
+   for(int i = 2 ; i<=n; i++) {
+      fact*=i;
+   }
+   return fact;
+}
  int main() {
-    string str;
-    cout<<" enter the string :";
-    getline(cin,str);
-    cout<<str<<"";
-    return 0;
+   int n = 4;
+  cout<< factorialN(n)<<endl;
+  return 0;
+  
  }
