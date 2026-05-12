64130f3469bfd6002076b7b1f2f1f5a0d9f007d7
Dev <dkothari703@gmail.com>
2026-05-12 18:12:56 +0530

    mode  unique and distinct elements

---
+++ maps=UNIQUEdistinctMODEOFelements.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//    int mode(vector<int> & nums ) {
+//     int n = nums.size();
+//     unordered_map<int, int> m;
+//     int freq=0; int element=nums[0];
+//      for(int i = 0 ;i< n; i++) {
+//       m[nums[i]]++;
+//         if(m[nums[i]]>freq) {
+//             freq=m[nums[i]];
+//             element=nums[i];
+//         }
+//         }
+//         return element;
+//      }
+   
+//  vector<int> uniqueElement(vector<int> & nums ){
+//     int n = nums.size();
+//      unordered_map<int, int> m;
+//       vector<int> ans;
+//       for(int i = 0 ; i< n;i++) {
+//         m[nums[i]]++;
+//       }
+//       for(int i= 0 ;i< n; i++) {
+//         if(m[nums[i]]==1) {
+//         ans.push_back(nums[i]);
+//       }
+// }
+// return ans;
+//  }
+  // vector<int> distinctElement(vector<int> & nums ){
+  //   int n = nums.size();
+  //    unordered_map<int, int> m;
+  //     vector<int> ans;
+  //     for(int i = 0 ; i< n;i++) {
+       
+  //       if(m[nums[i]]==0) 
+  //       {
+  //         ans.push_back(nums[i]);
+  //     }
+  //      m[nums[i]]++;
+
+  //   }
+  //     return ans;
+    
+  // }
+    
+  //   int main () {
+  //       vector<int> nums={1,2,3,3,5,5};
+        
+  //       vector<int> ans=distinctElement(nums);
+  //       for(int x:ans ) {
+  //           cout<<x;
+  //       }
+        
+        
+  //        return 0;
+  //   }
